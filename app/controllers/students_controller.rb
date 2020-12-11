e03d6be6cd197131a70a6bcb505aa55198815486
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-12-11 10:44:52 -0500

    commit tree 344daef515475a1cd93052cc082d933e8db23882

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,12 @@ class StudentsController < ApplicationController
   def new
   end
 
-  def create
+  def create 
+    @student = Student.create(
+      {first_name: params[:first_name],
+      last_name: params[:last_name]}
+    )
+    redirect_to student_path(@student)
   end
 
 end
